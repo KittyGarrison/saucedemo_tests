a909461313de2eb0f75b333552767fd3d65686d3
Kitty Katrina Garrison <katrinagarrison@Kittys-MacBook-Pro.local>
2024-01-29 23:45:02 -0500

    initiating cypress, setting base URL

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://www.saucedemo.com/",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
