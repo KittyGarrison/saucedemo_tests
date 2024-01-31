6b72893f623cebe03ef20d1054ac11c4ca431240
Kitty Katrina Garrison <katrinagarrison@Kittys-MacBook-Pro.local>
2024-01-31 03:28:32 -0500

    added custom command for login with default values

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,28 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+/**
+ * Custom command to select DOM elements by data-test attribute.
+ * @param {string} testId - The data-test attribute value to match.
+ * @returns {Cypress.Chainable<JQuery<HTMLElement>>}
+ */
+
+Cypress.Commands.add('getByDataTest', (testId) => {
+  return cy.get(`[data-test=${testId}]`);
+});
+
+/**
+ * Custom command to log in with provided credentials.
+ * @param {string} [username='standard_user'] - The username for login. Defaults to 'standard_user'.
+ * @param {string} [password='secret_sauce'] - The password for login. Defaults to 'secret_sauce'.
+ */
+
+Cypress.Commands.add(
+  'loginWithCredentials',
+  (username = 'standard_user', password = 'secret_sauce') => {
+    cy.getByDataTest('username').type(username);
+    cy.getByDataTest('password').type(password);
+    cy.getByDataTest('login-button').click();
+  }
+);
