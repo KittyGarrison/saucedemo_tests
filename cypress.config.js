c164bad885550153efedd03e2acb5c98e3d7c7f2
Kitty Katrina Garrison <katrinagarrison@Kittys-MacBook-Pro.local>
2024-01-31 03:30:35 -0500

    updated gitignore to ignore screenshots, formatted for single quotes

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://www.saucedemo.com/",
+    baseUrl: 'https://www.saucedemo.com/',
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
