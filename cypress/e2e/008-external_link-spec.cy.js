c7169184e77599c03dbe9e865744779e0b9a916a
Kitty Katrina Garrison <katrinagarrison@Kittys-MacBook-Pro.local>
2024-02-01 01:50:22 -0500

    added test for external link validation

---
+++ cypress/e2e/008-external_link-spec.cy.js
@@ -0,0 +1,39 @@
+describe('External Link', () => {
+
+    const externalLinks = [
+      {
+        selector: 'a#about_sidebar_link',
+        URL: 'https://saucelabs.com/',
+        name: 'About',
+      },
+      {
+        selector: '.social_twitter a',
+        URL: 'https://twitter.com/saucelabs',
+        name: 'Twitter',
+      },
+      {
+        selector: '.social_facebook a',
+        URL: 'https://www.facebook.com/saucelabs',
+        name: 'Facebook',
+      },
+      {
+        selector: '.social_linkedin > a',
+        URL: 'https://www.linkedin.com/company/sauce-labs/',
+        name: 'LinkedIn',
+      },
+    ];
+
+  externalLinks.forEach((link) => {
+
+    it(`contains correct URL for ${link.name}` , () => {
+      
+      // Navigate to site and login
+      cy.visit('/');
+      cy.loginWithCredentials();
+  
+      cy.get(link.selector).should('have.attr', 'href', link.URL);
+  
+    });
+
+  })
+});
