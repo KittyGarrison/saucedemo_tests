4c7c15c0c50185fe38f314698f25a3b6042c2084
Kitty Katrina Garrison <katrinagarrison@Kittys-MacBook-Pro.local>
2024-02-01 01:49:40 -0500

    added test for cart access

---
+++ cypress/e2e/004-cart_access-spec.cy.js
@@ -0,0 +1,20 @@
+describe('Access to cart page', () => {
+  it('is denied for unauthenticated user', () => {
+    cy.visit('/');
+    
+    // Login and add 2 items to cart
+    cy.loginWithCredentials();
+    cy.get('[data-test^=add-to-cart]').first().click();
+    cy.get('[data-test^=add-to-cart]').first().click();
+    cy.get('.shopping_cart_link').click();
+    cy.get('.cart_item').should('have.length', 2);
+    
+    // Logout
+    cy.get('a#logout_sidebar_link').click({ force: true });
+
+    // Access to shopping cart by url fails when logged out
+    cy.visit('/cart.html', { failOnStatusCode: false });
+    cy.getByDataTest('error').should('be.visible');
+    cy.location('pathname').should('equal', '/');
+  });
+});
