ffc1428a4a667986baf74a45e13f29db18efae33
Kitty Katrina Garrison <katrinagarrison@Kittys-MacBook-Pro.local>
2024-02-05 02:31:44 -0500

    Added 2 Percy tests

---
+++ cypress/e2e/009-PDP_visual_check-spec.cy copy.js
@@ -0,0 +1,11 @@
+describe('Product Detail Page', () => {
+
+  it(`renders and captures screenshot for review`, () => {
+    // Navigate to site and login
+    cy.visit('/');
+    cy.loginWithCredentials();
+
+    cy.get('.inventory_list').should('exist');
+    cy.percySnapshot('PDP responsive test', { widths: [768, 992, 1200] });
+  });
+});
